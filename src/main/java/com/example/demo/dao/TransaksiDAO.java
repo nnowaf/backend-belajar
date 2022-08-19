997076fd12675254563f7303e2db59430dbd33cd
Mohammad Nowaf <muhammadnowaf2608@gmail.com>
2022-08-19 10:58:59 +0700

    assignment magang final: - add column harga on table produk - create tabel transaksi - add foreign key table transaksi to table produk - create CRUD (findall, findbyid, create, update, delete) - create respone api detail transaksi
    update total harga

--- src/main/java/com/example/demo/dao/TransaksiDAO.java
+++ src/main/java/com/example/demo/dao/TransaksiDAO.java
@@ -9,12 +9,11 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.TransactionManagementConfigurationSelector;
+
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
